de830b15bb4e82496fcf0771d389ca01e1725cfe
VisionMise <geoffrey.kuhl@gmail.com>
2015-09-10 01:27:53 -0400

    Added create function

--- app/sys/handlers/ka10.handler.php
+++ app/sys/handlers/ka10.handler.php
@@ -4,7 +4,7 @@
 
 	function create(array $args = array()) {
 
-		$name 	= (isset($args[0])) 
+		$name 		= (isset($args[0])) 
 			? $args[0] 
 			: ((isset($args['name']))
 				? $args['name']
@@ -15,6 +15,24 @@
 		if (!$name) return 
 			"Handlers require a name\n".
 			"./ka10 handler.create name=myHandler\n"
+		;
+
+		$path 		= realpath('sys/handlers');
+		if (!$path) return
+			"No place to store handlers!\n"
+		;
+
+		$content 	= "<?php\n\tnamespace $name;\n?>";
+
+		$file 		= "sys/handlers/ka10.$name.php";
+		if (file_exists($file)) return
+			"Handler already exists!"
+		;
+
+		$wrote 		= file_put_contents($file, $content);
+
+		if (!$wrote) return
+			"Could not create file $file"
 		;
 
 		return "Handler '$name' Created.";
